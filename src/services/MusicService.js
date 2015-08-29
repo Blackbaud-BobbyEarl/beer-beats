d1960b5ded5251ed17cbc3116ab5a3378ef80f33
pofriel <ofrielp@gmail.com>
2015-08-29 09:00:00 -0400

    encodeURIComponent

--- src/services/MusicService.js
+++ src/services/MusicService.js
@@ -2,13 +2,15 @@
 (function () {
     'use strict';
 
-    function MusicService($http, $q) {
+    function MusicService($http, $q, MusicProxy) {
 
         var service = {};
 
         service.search = function (query) {
             var deferred = $q.defer();
-            $http.get('//api.spotify.com/v1/search?q=' + encodeURIComponent(query) + '&type=track').success(function (res) {
+            
+            query = encodeURIComponent(query);
+            $http.get(MusicProxy + encodeURIComponent('https://api.spotify.com/v1/search?q=' + query + '&type=track')).success(function (res) {
                 deferred.resolve(res);
             });
             return deferred.promise;
@@ -17,9 +19,9 @@
         service.getArtistByTrackId = function (trackId) {
             var deferred = $q.defer();
             var data = {};
-            $http.get('//api.spotify.com/v1/tracks/' + trackId).success(function (res) {
+            $http.get(MusicProxy + encodeURIComponent('https://api.spotify.com/v1/tracks/' + trackId)).success(function (res) {
                 data.track = res;
-                $http.get('//api.spotify.com/v1/artists/' + res.artists[0].id).success(function (res) {
+                $http.get(MusicProxy + encodeURIComponent('https://api.spotify.com/v1/artists/' + res.artists[0].id)).success(function (res) {
                     data.artist = res;
                     deferred.resolve(data);
                 });
@@ -33,10 +35,12 @@
 
     MusicService.$inject = [
         '$http',
-        '$q'
+        '$q',
+        'MusicProxy'
     ];
 
     angular.module('singingbeer')
-        .factory('MusicService', MusicService);
+        .factory('MusicService', MusicService)
+        .constant('MusicProxy', 'http://developer.blackbaud.com/proxy/?mode=native&url=');
 
 }());
