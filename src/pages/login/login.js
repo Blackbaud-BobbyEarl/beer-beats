ad84efdad941e6792ccb45931b0d36e23eef8f5c
Steve Brush <steve.brush@blackbaud.com>
2015-08-28 23:45:01 -0400

    Login controller music

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,10 +1,24 @@
 /*global angular */
 (function () {
     'use strict';
-    function LoginPageController() {
-        
+
+    function LoginPageController($location, MusicService) {
+        var vm = this;
+        vm.connect = function () {
+            console.log("Connecting...");
+            MusicService.connect().then(function (data) {
+                console.log("Successfully connected!", data);
+                $location.path('/beer');
+            });
+        };
     }
-    
+
+    LoginPageController.$inject = [
+        '$location',
+        'MusicService'
+    ];
+
     angular.module('singingbeer')
         .controller('LoginPageController', LoginPageController);
+
 }());
