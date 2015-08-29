fc232da3f2b79e3e2b35ac3f3dab8dea0cd05726
Bobby Earl <bobby@simplyearl.com>
2015-08-29 04:24:23 -0400

    Temporary commit in order to merge

--- src/pages/check-in/check-in.js
+++ src/pages/check-in/check-in.js
@@ -2,23 +2,65 @@
 (function () {
     'use strict';
 
-    function CheckInController($location, MusicService) {
+    function CheckInController($state, MusicService, BeerService) {
         var vm = this;
-        vm.query = "";
+        // vm.query = "";
+        // vm.search = function () {
+        //     MusicService.search(vm.query).then(function (data) {
+        //         console.log("Music retrieved: ", data);
+        //         vm.tracks = data.tracks;
+        //     });
+        // };
+        // vm.compare = function (trackId) {
+        //     console.log("Track ID: ", trackId);
+        //     $location.path('/beer/' + trackId);
+        // };
+
+        vm.searching = false;
+        vm.enjoying = 'beer';
+        vm.error = '';
+        vm.query = '';
+        vm.results = '';
+
         vm.search = function () {
-            MusicService.search(vm.query).then(function (data) {
-                console.log("Music retrieved: ", data);
-                vm.tracks = data.tracks;
+            vm.error = '';
+            vm.results = '';
+            vm.searching = true;
+            switch (vm.enjoying) {
+                case 'beer':
+                    BeerService.search(vm.query).then(success, error);
+                break;
+                case 'song':
+                    MusicService.search(vm.query).then(success, error);
+                break;
+            }
+        };
+
+        vm.recommend = function (id) {
+            $state.go('recommend', {
+                type: vm.enjoying,
+                id: id
             });
-        };
-        vm.compare = function (trackId) {
-            console.log("Track ID: ", trackId);
-            $location.path('/beer/' + trackId);
-        };
+        }
+
+        function success(results) {
+            vm.searching = false;
+            vm.results = results;
+            console.log(results);
+            vm.error = '';
+        }
+
+        function error(err) {
+            vm.searching = false;
+            vm.results = '';
+            vm.error = err;
+        }
+
     }
 
+
     CheckInController.$inject = [
-        '$location',
+        '$state',
         'MusicService'
     ];
 
